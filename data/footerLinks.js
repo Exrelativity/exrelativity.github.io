7a5ed4e4c2accc7e8783a7919dcd37f09af3fc6e
exrelativity <exrelativity@gmail.com>
2022-12-28 01:18:26 +0100

    initial commit

---
+++ data/footerLinks.js
@@ -0,0 +1,19 @@
+const links = [
+  {
+    label: "Github",
+    icon: "github",
+    url: "https://github.com/exrelativity/",
+  },
+  {
+    label: "LinkedIn",
+    icon: "linkedin",
+    url: "https://www.linkedin.com/in/everest-ukweh-354a1740/",
+  },
+  {
+    label: "Resume",
+    icon: "document",
+    url: "/resume.pdf",
+  },
+];
+
+export default links;
